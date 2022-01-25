f2909002504a09604af3b4c79f0a474fb2581b7d
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2022-01-25 12:22:41 +0200

    validator fix

--- src/Service.Verification.Api/Validators/PhoneVerifyValidator.cs
+++ src/Service.Verification.Api/Validators/PhoneVerifyValidator.cs
@@ -8,11 +8,15 @@ namespace Service.Verification.Api.Validators
     {
         public PhoneRequestValidator()
         {
+            RuleFor(x => x.PhoneCode).NotNull();
+
             RuleFor(x => x.PhoneCode).Matches("^[+]{0,1}[0-9]*$");
             RuleFor(x => x.PhoneCode).Length(1, 5);
             RuleFor(x => x.PhoneCode).Must(t => t.StartsWith('+')).WithMessage("Phone number should starts with +");
             RuleFor(x => x.PhoneCode).Must(t => !t.Any(char.IsWhiteSpace));
             
+            RuleFor(x => x.PhoneBody).NotNull();
+
             RuleFor(x => x.PhoneBody).Matches("^[0-9]*$");
             RuleFor(x => x.PhoneBody).Length(4, 20);
             RuleFor(x => x.PhoneBody).Must(t => !t.Any(char.IsWhiteSpace));
@@ -23,11 +27,15 @@ namespace Service.Verification.Api.Validators
     {
         public PhoneVerifyValidator()
         {
+            RuleFor(x => x.PhoneCode).NotNull();
+
             RuleFor(x => x.PhoneCode).Matches("^[+]{0,1}[0-9]*$");
             RuleFor(x => x.PhoneCode).Length(1, 5);
             RuleFor(x => x.PhoneCode).Must(t => t.StartsWith('+')).WithMessage("Phone number should starts with +");
             RuleFor(x => x.PhoneCode).Must(t => !t.Any(char.IsWhiteSpace));
             
+            RuleFor(x => x.PhoneBody).NotNull();
+
             RuleFor(x => x.PhoneBody).Matches("^[0-9]*$");
             RuleFor(x => x.PhoneBody).Length(4, 20); 
             RuleFor(x => x.PhoneBody).Must(t => !t.Any(char.IsWhiteSpace));
