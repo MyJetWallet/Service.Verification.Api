f153ff827f010ed265f4e15c06ed34b1b89be22c
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2022-01-25 13:47:33 +0200

    validator fix

--- src/Service.Verification.Api/Validators/PhoneVerifyValidator.cs
+++ src/Service.Verification.Api/Validators/PhoneVerifyValidator.cs
@@ -12,14 +12,14 @@ namespace Service.Verification.Api.Validators
 
             RuleFor(x => x.PhoneCode).Matches("^[+]{0,1}[0-9]*$");
             RuleFor(x => x.PhoneCode).Length(1, 5);
-            RuleFor(x => x.PhoneCode).Must(t => t.StartsWith('+')).WithMessage("Phone number should starts with +");
-            RuleFor(x => x.PhoneCode).Must(t => !t.Any(char.IsWhiteSpace));
+            RuleFor(x => x.PhoneCode).Must(t => t!= null && t.StartsWith('+')).WithMessage("Phone number should starts with +");
+            RuleFor(x => x.PhoneCode).Must(t => t!= null &&!t.Any(char.IsWhiteSpace));
             
             RuleFor(x => x.PhoneBody).NotNull();
 
             RuleFor(x => x.PhoneBody).Matches("^[0-9]*$");
             RuleFor(x => x.PhoneBody).Length(4, 20);
-            RuleFor(x => x.PhoneBody).Must(t => !t.Any(char.IsWhiteSpace));
+            RuleFor(x => x.PhoneBody).Must(t => t!= null && !t.Any(char.IsWhiteSpace));
         }
     }
     
@@ -31,14 +31,14 @@ namespace Service.Verification.Api.Validators
 
             RuleFor(x => x.PhoneCode).Matches("^[+]{0,1}[0-9]*$");
             RuleFor(x => x.PhoneCode).Length(1, 5);
-            RuleFor(x => x.PhoneCode).Must(t => t.StartsWith('+')).WithMessage("Phone number should starts with +");
-            RuleFor(x => x.PhoneCode).Must(t => !t.Any(char.IsWhiteSpace));
+            RuleFor(x => x.PhoneCode).Must(t => t!= null && t.StartsWith('+')).WithMessage("Phone number should starts with +");
+            RuleFor(x => x.PhoneCode).Must(t => t!= null && !t.Any(char.IsWhiteSpace));
             
             RuleFor(x => x.PhoneBody).NotNull();
 
             RuleFor(x => x.PhoneBody).Matches("^[0-9]*$");
             RuleFor(x => x.PhoneBody).Length(4, 20); 
-            RuleFor(x => x.PhoneBody).Must(t => !t.Any(char.IsWhiteSpace));
+            RuleFor(x => x.PhoneBody).Must(t => t!= null && !t.Any(char.IsWhiteSpace));
         }
     }
 }
