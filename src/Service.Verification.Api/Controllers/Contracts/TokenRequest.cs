e0103e5948b6c9d35e7daff4fc24d8c2d21c6a4d
Pavlo Pylypenko <pavel.p@smplt.net>
2022-02-17 12:27:32 +0200

    added OperationId to token verify contract

--- src/Service.Verification.Api/Controllers/Contracts/TokenRequest.cs
+++ src/Service.Verification.Api/Controllers/Contracts/TokenRequest.cs
@@ -3,5 +3,6 @@ namespace Service.Verification.Api.Controllers.Contracts
     public class TokenRequest
     {
         public string Token { get; set; }
+        public string OperationId { get; set; }
     }
 }
