b10b04122c5a6ba36d2cdf7bd72ac61e292e8a7d
Hays R <wonderhays@gmail.com>
2022-04-28 17:23:47 +0300

    verification api fixes

--- src/Service.Verification.Api/ApplicationLifetimeManager.cs
+++ src/Service.Verification.Api/ApplicationLifetimeManager.cs
@@ -8,25 +8,21 @@ namespace Service.Verification.Api
     public class ApplicationLifetimeManager : ApplicationLifetimeManagerBase
     {
         private readonly ILogger<ApplicationLifetimeManager> _logger;
-        private readonly MyNoSqlClientLifeTime _myNoSqlClientLifeTime;
 
-        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger, MyNoSqlClientLifeTime myNoSqlClientLifeTime)
+        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger)
             : base(appLifetime)
         {
             _logger = logger;
-            _myNoSqlClientLifeTime = myNoSqlClientLifeTime;
         }
 
         protected override void OnStarted()
         {
             _logger.LogInformation("OnStarted has been called.");
-            _myNoSqlClientLifeTime.Start();
         }
 
         protected override void OnStopping()
         {
             _logger.LogInformation("OnStopping has been called.");
-            _myNoSqlClientLifeTime.Stop();
         }
 
         protected override void OnStopped()
