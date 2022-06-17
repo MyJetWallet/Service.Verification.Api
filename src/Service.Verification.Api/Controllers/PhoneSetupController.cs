a0d58607d4398d304cc09ed0469be55f48c3bb18
Pavlo Pylypenko <pavel.p@smplt.net>
2022-06-17 17:31:08 +0300

    updated verification api

--- src/Service.Verification.Api/Controllers/PhoneSetupController.cs
+++ src/Service.Verification.Api/Controllers/PhoneSetupController.cs
@@ -68,10 +68,10 @@ namespace Service.Verification.Api.Controllers
             if(clientId == "73024fbcbf9c43bfb9feb67b2f453f8e")
                 return new Response(ApiResponseCodes.OperationBlocked);
  
-            // await _attemptService.TrackPhoneSetupAttempt(new TrackAttemptRequest
-            // {
-            //     ClientId = clientId
-            // });
+            await _attemptService.TrackPhoneSetupAttempt(new TrackAttemptRequest
+            {
+                ClientId = clientId
+            });
             
             var clientProfile = await _clientProfile.GetOrCreateProfile(new GetClientProfileRequest
             {
